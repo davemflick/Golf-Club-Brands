46b59e30c7ed2f3bb1caa23b90b2e85da1f57027
davemflick <davemflick@yahoo.com>
2017-06-04 16:10:54 -0400

    tried adding some comment update, didn't work

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.get("/", (req, res)=>{
 
 //INDEX ROUTE
 app.get("/brands", (req, res)=>{
-
 	Brand.find({}, (err, brand)=>{
+		brand = brand.sort((a,b)=> a.rank-b.rank);
 		return err ? console.log(err) : res.render("brands", {brands: brand});
 	})
 });
@@ -46,8 +46,10 @@ app.get("/brands/new", (req, res)=>{
 });
 
 
-//CREATE ROUTE
+
+//CREATE ROUTE --> New brand
 app.post("/brands", (req, res)=>{
+	if(req.body.image === ""){req.body.image = golfBrandSchema.image;}
 	Brand.create(req.body, (err, brand)=>{
 		if(err){
 			alert("something went wrong on submit");
@@ -59,6 +61,7 @@ app.post("/brands", (req, res)=>{
 	})
 
 });
+
 
 //SHOW ROUTE
 app.get("/brands/:id", (req,res)=>{
@@ -72,9 +75,55 @@ app.get("/brands/:id", (req,res)=>{
 	});
 });
 
+//EDIT UPDATE
+app.get("/brands/:id/edit", (req,res)=>{
+	Brand.findById(req.params.id, (err, brand)=>{
+		if(err){
+			alert("Something went wrong");
+			res.redirect("/brand");
+		} else {
+			res.render("editBrand", {brand: brand});
+		}
+	});
+});
 
+
+//UPDATE ROUTE
+app.put("/brands/:id", (req,res)=>{
+	Brand.findByIdAndUpdate(req.params.id, req.body, (err, brand)=>{
+		if(err){
+			alert("Something went wrong");
+			res.redirect("/brand");
+		} else {
+			if(req.body.comments){
+				brand.comments.push(req.body.comments);
+			}
+			res.redirect("/brands/" + req.params.id);
+		}
+	});
+});
+
+
+
+//DESTROY ROUTE
+app.delete("/brands/:id",(req,res)=>{
+	Brand.findByIdAndRemove(req.params.id, (err,brand)=>{
+		if(err){
+			console.log(err);
+			res.redirect("/brands");
+		} else {
+			res.redirect("/brands");
+		}
+	});
+});
 
 
 app.listen(3000 || process.env.PORT, process.env.IP, ()=>{
 	console.log("Golf Brands Server is connected");
 });
+
+
+
+
+
+
