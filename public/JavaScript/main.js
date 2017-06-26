3fc5c23e150cea4f80cf504abb548c37a25419cc
davemflick <davemflick@yahoo.com>
2017-06-26 10:51:12 -0400

    some margin fixes and automated instructions on home page

--- public/JavaScript/main.js
+++ public/JavaScript/main.js
@@ -1,6 +1,28 @@
 const instructions = document.getElementsByClassName("inst");
 
+console.log(instructions)
+
+let slideIn = 0;
+function instructionSlide(){
+	let inLen = instructions.length
+	for(let i=0; i< inLen; i++){
+		instructions[i].style.display = "none"
+	}
+	slideIn++;
+	if(slideIn > inLen){
+		slideIn = 1;
+	}
+	instructions[slideIn-1].style.display = "block";
+	setTimeout(instructionSlide, 2700)
+}
+
+instructionSlide();
+
+
+
+
 $(document).ready(function(){
+
 	$('.help').click(function(){
 		$('.ui.modal').modal('show');
 	})	
